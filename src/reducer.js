98ae7475ca1c8f8be489283ddbd3bd34f34f9ee4
Peter Tulala <peter.tulala@gmail.com>
2019-01-18 18:21:33 +0100

    Immutable fix

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,10 @@
 import { handleActions } from 'redux-actions'
+import { fromJS } from 'immutable';
 import * as actions from './actions'
 
-const initialState = {
+const initialState = fromJS({
   messages: []
-}
+})
 
 // Reducer
 export const reducer = handleActions({
@@ -11,22 +12,23 @@ export const reducer = handleActions({
     throw new Error('redux-flash: missing middleware. Did you remember to add it when initializing your store?')
   },
   [actions._addMessage]: (state, { payload }) => {
-    const messages = [ ...state.messages, payload ]
-    return { ...state, messages }
+    window.console.log('ADD MESSAGE');
+    window.console.log(payload);
+    window.console.log(state);
+    return state.set('messages', state.get('messages').merge(payload))
   },
   [actions.removeMessage]: (state, { payload: id }) => {
-    const messages = state.messages.filter(m => m.id !== id)
-    return { ...state, messages }
+    return state.set('messages', state.get('messages').filter(m => m.id !== id))
   },
   [actions.clearMessages]: (state) => {
-    return { ...state, messages: [] }
+    return state.set('messages', fromJS([]))
   },
 }, initialState)
 
 // Selectors
 export const getFlashMessages = (state) => {
   if (!state.get('flash')) throw new Error('redux-flash: state not found. Did you remember to attach the reducer at key `flash`?')
-  return [ ...state.get('flash').messages ]
+  return state.getIn(['flash', 'messages'])
 }
 
 export const getSuccessMessages = (state) => getFlashMessages(state).filter(m => !m.isError)
